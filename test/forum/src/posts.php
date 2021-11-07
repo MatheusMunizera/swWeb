09b87a961512a5d6daf532c631f8f265dc1eed45
MatheusMunizera <matheus.munizera@gmail.com>
2021-11-07 20:17:12 -0300

    Style: Separando CSS

--- test/forum/src/posts.php
+++ test/forum/src/posts.php
@@ -32,7 +32,6 @@
             <div class="row d-block ">
 
                 <!-- SEARCHBOX -->
-
                 <div class=" collapse d-md-none text-center" id="search">
                     <?php include  'components/searchbox.php' ?>
                 </div>
@@ -48,34 +47,12 @@
 
     <main role="main">
 
- 
-
     <div class="all-posts">
     <?php include  'components/article-post.php' ?>
     <?php include  'components/reverse-article-post.php' ?>
     </div>
-
-   
-
-    
-    
-   
-
         
     </main>
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
     <!-- Bootstrap JS  -->
